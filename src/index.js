9c6465102b1ed60febd5e620417ae14ac004828d
amiti-17 <timotey0412@gmail.com>
2022-08-13 00:51:02 +0300

    create a function for JSON round exept.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-document.write('All right');
+const room = {
+  number: 23,
+};
+
+const meetup = {
+  title: 'Совещание',
+  occupiedBy: [{ name: 'Иванов' }, { name: 'Петров' }],
+  place: room,
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+// eslint-disable-next-line no-undef
+document.write(
+  JSON.stringify(
+    meetup,
+    (key, value) => {
+      console.log(`key: ${key}  value: ${value}`);
+      return key !== '' && value === meetup ? undefined : value;
+    },
+    2,
+  ),
+);
+
+// в результате должно быть:
+// {
+// "title":"Совещание",
+// "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+// "place":{"number":23}
+// }
+// const backjson = JSON.parse(jsonstr);
+// console.log(backjson);
