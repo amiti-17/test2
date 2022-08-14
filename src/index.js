8ef82a64c44a73ad15dbba971dbeb7031bd12aea
amiti-17 <timotey0412@gmail.com>
2022-08-14 14:48:50 +0300

    incorrect way for functions: inArray and inBetween

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-function sum(a) {
-  return function bb(b) {
-    return a + b;
-  };
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+function inBetween(arri, x, y) {
+  if(x <= arri && arri <= y) {
+    console.log("yes")
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(sum(2)(3));
+function inArray(arri, array) {
+  return array.includes(arri);
+}
+
+const result1 = arr.filter(
+  (arri) => {return inBetween(arri, 3, 6)}
+); // 3,4,5,6
+console.log(result1);
+const result2 = arr.filter(
+  (arri) => {return inArray(arri, [1, 2, 10])}
+);
+console.log(result2); // 1,2
