81d93bf775a7980360b7ef60d654afe235409cd5
amiti-17 <timotey0412@gmail.com>
2022-08-14 17:00:42 +0300

    create function counter()

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-function makeArmy() {
-  let shooters = [];
-
-  let i = 0;
-  while (i < 10) {
-    const a = i;
-    const shooter = function() { // функция shooter
-      // должна выводить порядковый номер
-      console.log(a);
-    };
-    shooters.push(shooter);
-    i++;
-  }
-
-  return shooters;
+function makeCounter() {
+  let counter = 0;
+  return (function() {
+    return counter++;
+  });
 }
-
-const army = makeArmy();
-
-army[0](); // у 0-го стрелка будет номер 10
-army[5](); // и у 5-го стрелка тоже будет номер 10
-// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
+const a = makeCounter();
+const b = makeCounter();
+const c = makeCounter();
+console.log('a', a());
+console.log('b', b());
+console.log('a', a());
+console.log('a', a());
+console.log('b', b());
+console.log('c', c());
+console.log('c', c());
