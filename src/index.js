fce7b88862f58892960942c54ac7bb6d3c4a284a
amiti-17 <timotey0412@gmail.com>
2022-08-14 03:20:09 +0300

    strange gunction

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-const list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null,
-      },
-    },
-  },
-};
-// eslint-disable-next-line no-shadow
-function listreader(list) {
-  if (list.next) {
-    listreader(list.next);
-    console.log(list.value);
-  } else {
-    console.log(list.value);
-  }
+function sum(a) {
+  return function bb(b) {
+    return a + b;
+  };
 }
-let tmp = list;
-let arr = [];
-while (tmp) {
-  arr.push(tmp.value);
-  tmp = tmp.next;
-}
-for (let i = arr.length - 1; i >= 0; i--) {
-  document.write(arr[i])
-}
-listreader(list);
+console.log(sum(2)(3));
