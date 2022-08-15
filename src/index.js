34c9dbd9eb719ede97551a83cdef00c171430bb8
amiti-17 <timotey0412@gmail.com>
2022-08-15 03:10:46 +0300

    create new interesting function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 function sum(a) {
-  return function bb(b) {
-    return a + b;
+  let currentSum = a;
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+  f.toString = function() {
+    return currentSum;
   };
+  return f;
 }
-console.log(sum(2)(3));
+console.log( sum(1)(2).toString()); // 3
+document.write( sum(5)(-1)(2) + "<br>"); // 6
+document.write( sum(6)(-1)(-2)(-3) + "<br>"); // 0
+document.write( sum(0)(1)(2)(3)(4)(5) + "<br>"); // 15
