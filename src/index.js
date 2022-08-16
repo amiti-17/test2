d00bafa3c584faf865ddd56bd03a39ab8d72308e
amiti-17 <timotey0412@gmail.com>
2022-08-16 03:23:00 +0300

    create function: printNumbers(from, to), with delay 1s

--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
-function makeCounter() {
-  let count = 0;
-  function counter() {
-    return count++;
-  };
-  counter.set = function(x) {
-    count = x;
-    return count;
+function printNumbers(from, to) {
+  function subprintNumbers(i) {
+    const idinterval = setInterval(() => {
+      if (i <= to) {
+        document.write(i++, '<br>');
+      } else {
+        clearInterval(idinterval);
+      }
+    }, 1000);
   }
-  counter.decrease = function() {
-    return --count;
-  }
-  return counter;
+  return subprintNumbers(from);
 }
 
-let counter = makeCounter();
-
-alert( counter() ); // 0
-alert( counter() ); // 1
-
-counter.set(10); // установить новое значение счётчика
-
-alert( counter() ); // 10
-
-counter.decrease(); // уменьшить значение счётчика на 1
-
-alert( counter() ); // 10 (вместо 11)
-
-// =======
-// function sum(a) {
-//   let currentSum = a;
-//   function f(b) {
-//     currentSum += b;
-//     return f;
-//   }
-//   f.toString = function() {
-//     return currentSum;
-//   };
-//   return f;
+// function printNumbers(from, to) {
+//   setTimeout(function run() {
+//     if (from <= to) document.write(from++, '<br>'); setTimeout(run, 1000);
+//   }, 1000);
 // }
-// console.log( sum(1)(2).toString()); // 3
-// document.write( sum(5)(-1)(2) + "<br>"); // 6
-// document.write( sum(6)(-1)(-2)(-3) + "<br>"); // 0
-// document.write( sum(0)(1)(2)(3)(4)(5) + "<br>"); // 15
-// >>>>>>> master
+
+printNumbers(1, 10);
