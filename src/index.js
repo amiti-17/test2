546970b6fac417d9ba7d9e6ae30a5401a5ba58af
amiti-17 <timotey0412@gmail.com>
2022-09-03 17:32:32 +0300

    rewrite Clock in class style

--- src/index.js
+++ src/index.js
@@ -1,30 +1,73 @@
-let dictionary = Object.create(null, {toString: {
-  "writable": true,
-  "configurable": true,
-  value() {return Object.keys(this).join(', ')},
-}});
+// class Clock{
+//   constructor (template) {
+//     let timer;
+//     this.render = function () {
+//       let date = new Date();
 
-// ваш код, который добавляет метод dictionary.toString
+//       let hours = date.getHours();
+//       if (hours < 10) hours = '0' + hours;
 
-// Object.defineProperty(dictionary, "toString", {
-//   "writable": true,
-//   "enumerable": false,
-//   "configurable": true,
-// });
+//       let mins = date.getMinutes();
+//       if (mins < 10) mins = '0' + mins;
 
-// dictionary.toString = function() {
-//   return Object.keys(this).join(', ')
+//       let secs = date.getSeconds();
+//       if (secs < 10) secs = '0' + secs;
+
+//       let output = template
+//         .replace('h', hours)
+//         .replace('m', mins)
+//         .replace('s', secs);
+
+//       console.log(output + ": clock 22");
+//     };
+//     this.start = function() {
+//       this.render();
+//       timer = setInterval(this.render, 1000);
+//     };
+//     this.stop = function() {
+//       clearInterval(timer);
+//     };
+//   }
 // }
 
-// добавляем немного данных
-dictionary.apple = "Apple";
-dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
-
-// только apple и __proto__ выведены в цикле
-for(let key in dictionary) {
-  console.log(key); // "apple", затем "__proto__"
-}
 
 
-// ваш метод toString в действии
-alert(dictionary); // "apple,__proto__"
+// let clock = new Clock('h:m:s');
+// console.log(clock)
+// clock.start();
+
+// function Clock1({ template }) {
+  
+//   let timer;
+
+//   function render() {
+//     let date = new Date();
+
+//     let hours = date.getHours();
+//     if (hours < 10) hours = '0' + hours;
+
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = '0' + mins;
+
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = '0' + secs;
+
+//     let output = template
+//       .replace('h', hours)
+//       .replace('m', mins)
+//       .replace('s', secs);
+
+//     console.log(output + ": clock 1");
+//   }
+
+//   this.stop = function() {
+//     clearInterval(timer);
+//   };
+
+//   this.start = function() {
+//     render();
+//     timer = setInterval(render, 1000);
+//   };
+
+// }
+h
