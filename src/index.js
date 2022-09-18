a554de87e852bfbd32799dacd793eb284373ff01
amiti-17 <timotey0412@gmail.com>
2022-09-19 01:22:15 +0300

    resolve task from kata: rgb()

--- src/index.js
+++ src/index.js
@@ -1,81 +1,38 @@
-function concatination(stringA, stringB) {
-  return stringA + stringB;
+function rgb(r, g, b) {
+  r = valid(r);
+  g = valid(g);
+  b = valid(b);
+  r = convectorFrom10To16(r);
+  g = convectorFrom10To16(g);
+  b = convectorFrom10To16(b);
+  return r + g + b;
 }
-
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-  }
-
-  render() {
-    const date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = concatination('0', hours);
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = concatination('0', mins);
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = concatination('0', secs);
-
-    const output = this.template
-      .replace('h', hours)
-      .replace('m', mins)
-      .replace('s', secs);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 1000);
-  }
+function valid(number) {
+  return number < 0 ? 0 : number > 255 ? 255 : number;
 }
-
-class ExtendedClock extends Clock {
-  constructor(option) {
-    super(option);
-    const { precision = 1000 } = option;
-    this.precision = precision;
-  }
-
-  start() {
-    this.render();
-
-    this.timer = setInterval(() => this.render(), this.precision);
-  }
+function convectorFrom10To16(number) {
+  arr = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+  let a = Math.floor(number / 16);
+  a = arr[a];
+  let b = number % 16;
+  b = arr[b];
+  return a + b;
 }
-
-class ExtendedClock2 extends ExtendedClock {
-  constructor(option) {
-    super(option);
-    const { third = 1000 } = option;
-    this.third = third;
-  }
-
-  start() {
-    this.render();
-
-    this.timer = setInterval(() => this.render(), this.precision + this.third);
-  }
-}
-
-const lowResolutionClock = new ExtendedClock({
-  template: 'h:m:s',
-  precision: 1000,
-});
-
-lowResolutionClock.start();
-
-const middleResolutionClock = new ExtendedClock2({
-  template: 'h:m:s',
-  precision: 5000,
-  fourth: 1000,
-});
-
-//middleResolutionClock.start();
+console.log(rgb(45, -8, 899));
