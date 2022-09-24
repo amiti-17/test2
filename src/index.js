3467b5fbec2ee436648180a885095a55b73b772e
amiti-17 <timotey0412@gmail.com>
2022-09-24 16:51:28 +0300

    create pseudo-random generator, based on formula with * and %

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
-function rgb(r, g, b) {
-  r = valid(r);
-  g = valid(g);
-  b = valid(b);
-  r = convectorFrom10To16(r);
-  g = convectorFrom10To16(g);
-  b = convectorFrom10To16(b);
-  return r + g + b;
+function* pseudoRandom(seed) {
+  while (true) {
+    seed = (seed * 16807) % 2147483647;
+    yield seed;
+  }
 }
-function valid(number) {
-  return number < 0 ? 0 : number > 255 ? 255 : number;
-}
-function convectorFrom10To16(number) {
-  arr = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-  ];
-  let a = Math.floor(number / 16);
-  a = arr[a];
-  let b = number % 16;
-  b = arr[b];
-  return a + b;
-}
-console.log(rgb(45, -8, 899));
+let generator = pseudoRandom(1);
+
+alert(generator.next().value); // 16807
+alert(generator.next().value); // 282475249
+alert(generator.next().value); // 1622650073
