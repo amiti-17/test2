3728e43ce98f266df5f76fdb5dcac4bb41264b94
amiti-17 <timotey0412@gmail.com>
2022-10-12 15:34:50 +0300

    resolve task: Error on reading non-existent property

--- src/index.js
+++ src/index.js
@@ -1,62 +1,23 @@
-function aclean(arr) {
-  let myArr = [];
-  for (let j = 0; j < arr.length; j++) {
-    let currentMap = convertWordIntoMap(arr[j]);
-    let checker = [];
-    if (myArr.length == 0) {
-      myArr.push(arr[j]);
-    } else {
-      for (let i = 0; i < myArr.length; i++) {
-        let oneOfUniqMap = convertWordIntoMap(myArr[i]);
-        checker.push(compareMapBool(currentMap, oneOfUniqMap));
-      }
-      let checkerArr = checker.filter((check) => (check ? true : false));
-      if (checkerArr[0]) {
-        continue;
+let user = {
+  name: 'John',
+};
+
+function wrap(target) {
+  return new Proxy(target, {
+    get(target, prop, reciever) {
+      if (Reflect.has(...arguments)) {
+        return Reflect.get(...arguments);
       } else {
-        myArr.push(arr[j]);
+        throw new Error(`Свойство не существует: "${prop}"`);
       }
-    }
-  }
-  return myArr;
-}
-
-function convertWordIntoMap(word) {
-  word = word.toLowerCase().split("");
-  let currentMap = new Map();
-  word.forEach((element) => {
-    if (currentMap.get(element)) {
-      currentMap.set(element, currentMap.get(element) + 1);
-    } else {
-      currentMap.set(element, 1);
-    }
+    },
   });
-  return currentMap;
 }
 
-function compareMapBool(map1, map2) {
-  let check1 = false;
-  let check2 = false;
-  for (const elem of map1.keys()) {
-    if (map2.has(elem) && map2.get(elem) === map1.get(elem)) {
-      check1 = true;
-    } else {
-      check1 = false;
-      return false;
-    }
-  }
-  for (const elem of map2.keys()) {
-    if (map1.has(elem) && map1.get(elem) === map2.get(elem)) {
-      check2 = true;
-    } else {
-      check2 = false;
-      return false;
-    }
-  }
-
-  return check1 && check2 ? true : false;
+user = wrap(user);
+try {
+  console.log(user.name); // John
+  console.log(user.age); // Ошибка: такого свойства не существует
+} catch (error) {
+  console.log(error.name);
 }
-
-let values = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-
-console.log(aclean(values));
