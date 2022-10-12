bd7be7cd982128f670acd4f7df5db8f65e721c5a
amiti-17 <timotey0412@gmail.com>
2022-10-13 00:44:24 +0300

    resolve task: Eval-калькулятор

--- src/index.js
+++ src/index.js
@@ -1,36 +1,3 @@
-const handlers = Symbol('handlers');
-
-function makeObservable(target) {
-  target[handlers] = [];
-
-  target.observe = function (handler) {
-    this[handlers].push(handler);
-  };
-  return new Proxy(target, {
-    // get(target, prop, reciever) {
-    //   let value = Reflect.get(...arguments);
-    //   // target.observe(prop, value);
-    //   return value;
-    // },
-    set(target, prop, value, reciever) {
-      let result = Reflect.set(...arguments);
-      if (result) {
-        target[handlers].forEach((handler) => {
-          handler(prop, value);
-        });
-      }
-      return result;
-    },
-  });
-}
-
-let user = {};
-
-user = makeObservable(user);
-
-user.observe((key, value) => {
-  alert(`SET ${key}=${value}`);
-});
-
-user.name = 'John'; // alerts: SET name=John
-user.name = 'Tim';
+let a = prompt('Give me some expresion, i`ll try to count it)', '2*3-5');
+a = new Function(`return ${a}`);
+console.log(a());
