c3d10165b7b233f27b5de74c248c8becc6f5d77f
amiti-17 <timotey0412@gmail.com>
2022-10-14 15:32:16 +0300

    resolve task: Отсортируйте массив с буквой ё

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-let a = prompt('Give me some expresion, i`ll try to count it)', '2*3-5');
-a = new Function(`return ${a}`);
-console.log(a());
+let animals = ['тигр', 'ёж', 'еж', 'енот', 'ехидна', 'АИСТ', 'ЯК'];
+
+const collator = new Intl.Collator('ru', {
+  localeMatcher: 'best fit',
+  usage: 'sort',
+  sensitivity: 'base',
+});
+const collatorVariant = new Intl.Collator('ru', {
+  localeMatcher: 'best fit',
+  usage: 'sort',
+  sensitivity: 'variant',
+});
+
+animals = animals.sort((word1, word2) => {
+  return collator.compare(word1, word2)
+    ? collator.compare(word1, word2)
+    : collatorVariant.compare(word1, word2);
+});
+
+console.log(animals); // АИСТ,ёж,енот,ехидна,тигр,ЯК
