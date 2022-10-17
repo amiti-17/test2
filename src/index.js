e9abc12f41205a72a8790bc5a0293bfb84bacabd
amiti-17 <timotey0412@gmail.com>
2022-10-17 22:48:58 +0300

    resolve task: Выделите ячейки по диагонали

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-let animals = ['тигр', 'ёж', 'еж', 'енот', 'ехидна', 'АИСТ', 'ЯК'];
-
-const collator = new Intl.Collator('ru', {
-  localeMatcher: 'best fit',
-  usage: 'sort',
-  sensitivity: 'base',
-});
-const collatorVariant = new Intl.Collator('ru', {
-  localeMatcher: 'best fit',
-  usage: 'sort',
-  sensitivity: 'variant',
-});
-
-animals = animals.sort((word1, word2) => {
-  return collator.compare(word1, word2)
-    ? collator.compare(word1, word2)
-    : collatorVariant.compare(word1, word2);
-});
-
-console.log(animals); // АИСТ,ёж,енот,ехидна,тигр,ЯК
+let table = document.body.firstElementChild;
+for (let i = 0; i <= Array.from(table.rows).length; i++) {
+  table.rows[i].cells[i].style.backgroundColor = 'red';
+}
