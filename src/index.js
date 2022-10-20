988b257b304158a563e04c79fb3113a6567c6ad4
amiti-17 <timotey0412@gmail.com>
2022-10-20 17:36:58 +0300

    rosolve task: Выведите список потомков в дереве

--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
-let ul = document.createElement('ul')
-document.body.prepend(ul)
-ul.id = 'id55'
-let bool = true
-while (bool) {
-  let value = prompt('Enter data for list...', 'data')
-  if (value) {
-    let li = document.createElement('li')
-    li.textContent = value
-    console.log(value)
-    ul.append(li)
-  } else {
-    console.log(value)
-    bool = false
+let data = {
+  "Рыбы": {
+    "форель": {},
+    "лосось": {}
+  },
+
+  "Деревья": {
+    "Огромные": {
+      "секвойя": {},
+      "дуб": {}
+    },
+    "Цветковые": {
+      "яблоня": {},
+      "магнолия": {}
+    }
   }
 }
+let div = document.createElement('div')
+div.id = 'div'
+document.body.append(div)
+
+function createTree(container, data) {
+  let ul = document.createElement('ul')
+  for (let elem in data) {
+    console.log(elem)
+    if (isEmpty(data[elem])) {
+      let li = document.createElement('li')
+      li.textContent = elem
+      ul.append(li)
+    } else {
+      let li = document.createElement('li')
+      li.textContent = elem
+      console.log(li)
+      ul.append(li)
+      createTree(ul, data[elem])
+    }
+  }
+
+  container.append(ul)
+}
+
+
+function isEmpty(obj) {
+  for (let elem in obj) {
+    if (obj[elem]) { return false }
+  }
+  return true;
+}
+
+
+createTree(div, data)
+
+
+
+
+
+
+
+
+
+
