c088730f2009cc75787a13a92cf109db53f22290
amiti-17 <timotey0412@gmail.com>
2022-10-21 17:54:41 +0300

    rosolve task: Цветные часы с использованием setInterval. My first live clock)

--- src/index.js
+++ src/index.js
@@ -1,61 +1,54 @@
-let data = {
-  "Рыбы": {
-    "форель": {},
-    "лосось": {}
-  },
+const div = document.createElement('div');
+const buttonStart = document.createElement('button');
+const buttonStop = document.createElement('button');
+let date = new Date();
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
+let timer = 0;
 
-  "Деревья": {
-    "Огромные": {
-      "секвойя": {},
-      "дуб": {}
-    },
-    "Цветковые": {
-      "яблоня": {},
-      "магнолия": {}
-    }
-  }
-}
-let div = document.createElement('div')
-div.id = 'div'
-document.body.append(div)
-
-function createTree(container, data) {
-  let ul = document.createElement('ul')
-  for (let elem in data) {
-    console.log(elem)
-    if (isEmpty(data[elem])) {
-      let li = document.createElement('li')
-      li.textContent = elem
-      ul.append(li)
-    } else {
-      let li = document.createElement('li')
-      li.textContent = elem
-      console.log(li)
-      ul.append(li)
-      createTree(ul, data[elem])
-    }
-  }
-
-  container.append(ul)
+function resetTimer(timer) {
+  clearInterval(timer)
 }
 
-
-function isEmpty(obj) {
-  for (let elem in obj) {
-    if (obj[elem]) { return false }
-  }
-  return true;
+function refreshSecond(second) {
+  let spanSecond = document.createElement('span');
+  spanSecond.innerHTML = second;
+  spanSecond.style.color = "blue";
+  div.lastElementChild.replaceWith(spanSecond)
 }
 
+function refreshMinute(minute) {
+  let spanMinute = document.createElement('span');
+  spanMinute.innerHTML = minute;
+  spanMinute.style.color = "green";
+  div.lastElementChild.previousElementSibling.replaceWith(spanMinute)
+}
 
-createTree(div, data)
+function refreshHour(hour) {
+  let spanHour = document.createElement('span');
+  spanHour.innerHTML = hour;
+  spanHour.style.color = "red";
+  div.firstElementChild.replaceWith(spanHour)
+}
 
+document.body.append(div, buttonStart, buttonStop)
+div.innerHTML = '<span>hour</span>:<span>minutes</span>:<span>seconds</span>'
 
+buttonStop.innerHTML = 'Stop';
+buttonStart.innerHTML = 'Start';
+buttonStop.onclick = () => { resetTimer(timer); timer = 0 }
+buttonStart.onclick = () => {
+  if (timer) { } else {
+    timer = setInterval(() => {
+      date = new Date();
+      second = date.getSeconds();
+      refreshHour(hour);
+      refreshMinute(minute);
+      refreshSecond(second);
+      console.log(hour, minute, second);
+    }, 1000);
+  }
+}
 
-
-
-
-
-
-
-
+console.log(hour, minute, second)
