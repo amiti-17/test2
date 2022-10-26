a3def207d60dc322f691e5e7abb93a60aa3f86f5
amiti-17 <timotey0412@gmail.com>
2022-10-26 18:11:00 +0300

    resolve task: Поймайте переход по ссылке

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-let table = document.getElementById('grid')
-let collator = new Intl.Collator("ru-RU", { numeric: true })
-table.addEventListener('click', function (event) {
-  if (event.target.tagName != 'TH') { return }
-  let arr = []
-  let arrI = [];
-  for (let elem of event.target.parentNode.children) {
-    arrI.push(elem.innerHTML)
+contents.addEventListener('click', function (event) {
+  if (event.target.closest('a')) {
+    let conf = confirm(`Are you go to the ${event.target.closest('a').getAttribute('href')}`)
+    console.log(conf)
+    if (!conf) event.preventDefault();
   }
-  let indexOfSortColumn = arrI.indexOf(event.target.innerHTML)
-  console.log()
-  for (let elem of table.lastElementChild.children) {
-    arr.push(elem);
-  }
-  console.log(arr[0].children[indexOfSortColumn])
-  arr.sort((first, second) => {
-    console.log(first.children[indexOfSortColumn].innerHTML, second.children[indexOfSortColumn].innerHTML, collator.compare(first.children[indexOfSortColumn].innerHTML, second.children[indexOfSortColumn].innerHTML))
-    return collator.compare(first.children[indexOfSortColumn].innerHTML, second.children[indexOfSortColumn].innerHTML)
-  })
-  arr.forEach((el) => table.lastElementChild.append(el))
-  //table.lastElementChild.prepend(arr)
-  console.log(table.lastElementChild)
-  console.log(arr)
 })
