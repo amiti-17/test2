82df1cf0839ba8d030b9cb1bacb45a98d7d78307
amiti-17 <timotey0412@gmail.com>
2022-10-27 19:41:53 +0300

    resolve task: Отследить одновременное нажатие

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-contents.addEventListener('click', function (event) {
-  if (event.target.closest('a')) {
-    let conf = confirm(`Are you go to the ${event.target.closest('a').getAttribute('href')}`)
-    console.log(conf)
-    if (!conf) event.preventDefault();
-  }
+//document.addEventListener('keydown', function (event)) { }
+let pressed = new Set();
+document.addEventListener('keydown', function (event) {
+  console.log('pressed: ', event.code);
+  pressed.add(event.code);
+  runOnKeys(
+    () => alert("Привет!"),
+    "KeyQ",
+    "KeyW")
+  //console.log(pressed)
 })
+document.addEventListener('keyup', function (event) {
+  console.log('unpressed: ', event.code);
+  pressed.delete(event.code);
+  //console.log(pressed)
+})
+function runOnKeys(func, ...keys) {
+  let arr = Array.from(keys);
+  if (arr.every((el) => pressed.has(el))) {
+
+    func()
+  }
+}
+//document.body.addEventListener('keydown', runOnKeys(alert('cool'), 'KeyQ', 'KeyO'))
+function checked() {
+  console.log('good')
+}
