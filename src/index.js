690f095f2becc52d4c5b9f4c61573d34c501e4a3
amiti-17 <timotey0412@gmail.com>
2022-10-28 03:25:08 +0300

    resolve task: Отследить одновременное нажатие (in correct way)

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
-//document.addEventListener('keydown', function (event)) { }
-let pressed = new Set();
-document.addEventListener('keydown', function (event) {
-  console.log('pressed: ', event.code);
-  pressed.add(event.code);
-  runOnKeys(
-    () => alert("Привет!"),
-    "KeyQ",
-    "KeyW")
-  //console.log(pressed)
-})
-document.addEventListener('keyup', function (event) {
-  console.log('unpressed: ', event.code);
-  pressed.delete(event.code);
-  //console.log(pressed)
-})
-function runOnKeys(func, ...keys) {
-  let arr = Array.from(keys);
-  if (arr.every((el) => pressed.has(el))) {
 
-    func()
-  }
+function runOnKeys(func, ...codes) {
+  let pressed = new Set();
+
+  document.addEventListener('keydown', function (event) {
+    pressed.add(event.code);
+    for (let code of codes) {
+      if (!pressed.has(code)) {
+        return
+      };
+    }
+    pressed.clear();
+    func();
+  })
+  document.addEventListener('keyup', function (event) {
+    pressed.delete(event.code)
+  })
 }
-//document.body.addEventListener('keydown', runOnKeys(alert('cool'), 'KeyQ', 'KeyO'))
-function checked() {
-  console.log('good')
-}
+
+runOnKeys(() => alert("Привет!"), "KeyQ", "KeyW")
